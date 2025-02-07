715a3bba1c6c8e57ff9d886962a45733b3915562
edsonbassani <edsonbassani@gmail.com>
2025-02-07 05:35:36 +0000

    header

--- CartasSuperTrunfo.h
+++ CartasSuperTrunfo.h
@@ -1,5 +1,5 @@
 #pragma once
 void cabecalho();
+void gerarCodigoCarta(char codigoEstado, int codigoCidade, char nomeCidade[100], char uf[3]);
 double calcDensidadePopulacional(double populacao, double area);
 double calcPibPerCapita(double pib, double populacao);
-void gerarCodigoCarta(char codigoEstado, int codigoCidade, char nomeCidade[100], char uf[3]);
